04c74802d3f54153da3d91d5df5dabaac11beb58
Elina <elina815@gmail.com>
2020-01-31 23:14:31 +0200

    edited gitignore

--- gj-vln-2020/Assets/Scripts/MainMenuButtons.cs
+++ gj-vln-2020/Assets/Scripts/MainMenuButtons.cs
@@ -1,18 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuButtons : MonoBehaviour
 {
+    [SerializeField]
+    private Button StartBtn;
+    [SerializeField]
+    private Button OptionsBtn;
+    [SerializeField]
+    private Button ExitBtn;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //if save file found change start btn text
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartClicked()
     {
-        
+        //load next scene
+    }
+
+    public void OptionsClicked()
+    {
+        //load options menu
+    }
+
+    public void ExitClicked()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 }
