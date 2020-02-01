9973734a7e1d3482243ae549a1ba1ee1d05e09b4
EnZeeHI <34064073+EnZeeHI@users.noreply.github.com>
2020-02-01 07:13:17 +0200

    Sprint stamina done

--- gj-vln-2020/Assets/Scripts/CharacterMovement.cs
+++ gj-vln-2020/Assets/Scripts/CharacterMovement.cs
@@ -5,16 +5,24 @@ using UnityEngine.AI;
 
 public class CharacterMovement : MonoBehaviour
 {
-
     public LayerMask whatCanBeClickedOn;
     public int MaxStamina;
-    public int CurrentStamina;
-    public int StaminaRechargeDelay;
-    public int StaminaRechargeRate;
+    public float CurrentStamina;
+    public float StaminaRechargeDelayTimer;
+    public float StaminaRechargeTimeToDelay;
+    public float StaminaRechargeRate;
+    public float StaminaDecreasePerFrame;
+    public float StaminaIncreasePerFrame;
+     private float StaminaTimeToRegen = 3.0f;
     private NavMeshAgent CharacterAgent;
 
     bool NeedsToWalk;
 
+    bool IsRunning;
+
+    Rect StaminaRectangle;
+    Texture2D StaminaTexture;
+    
     Animator CharacterAnimator;
     // assigning variable values
     void Start()
@@ -22,33 +30,26 @@ public class CharacterMovement : MonoBehaviour
         CharacterAgent = GetComponent<NavMeshAgent>();
         CharacterAnimator = GetComponent<Animator>();
         CurrentStamina = MaxStamina;
-        //NeedsToWalk = false;
+        StaminaRectangle = new Rect(Screen.width / 10, Screen.height *9/10, Screen.width /3, Screen.height / 50);
+        StaminaTexture = new Texture2D(1,1);
+        StaminaTexture.SetPixel(0,0, Color.black);
+        StaminaTexture.Apply();
     }
-
     // Update is called once per frame
     void Update()
     {   // casting ray once from the mouse position
         Ray MouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-     
-        
         //checking for collisions
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            RaycastHit MouseHit;
-            
+            Walking(MouseRay);
             // Checking if the player can go to the collision point, setting navmesh agent destination, playing walking animation.
-            if (Physics.Raycast(MouseRay, out MouseHit, 100, whatCanBeClickedOn))
-            {  
-                CharacterAgent.SetDestination(MouseHit.point);
-                CharacterAnimator.CrossFadeInFixedTime("Walking",0f);
-                CharacterAgent.speed = 10;
-                NeedsToWalk = true;
-            }    
         }
         if (Input.GetKeyDown(KeyCode.Mouse1))
         {
             Running(MouseRay);
         }
+        else
         // checking if the player reached the destination and switching to idle animation
         if (!CharacterAgent.pathPending)
         {   
@@ -58,12 +59,23 @@ public class CharacterMovement : MonoBehaviour
                 {
                     CharacterAnimator.CrossFadeInFixedTime("Idle",0f);
                     NeedsToWalk = false;
+                    IsRunning = false;
                 }
-               
             }
         } 
+        StaminaRecharge();
     }
-
+    void Walking(Ray ray)
+    {   RaycastHit MouseHit;
+        if (Physics.Raycast(ray, out MouseHit, 100, whatCanBeClickedOn))
+        {  
+            CharacterAgent.SetDestination(MouseHit.point);
+            CharacterAnimator.CrossFadeInFixedTime("Walking",0f);
+            NeedsToWalk = true;
+            IsRunning =false;
+            
+        }    
+    }
     void Running(Ray ray)
     {    RaycastHit MouseHit;
         if (CurrentStamina > 1)
@@ -73,12 +85,39 @@ public class CharacterMovement : MonoBehaviour
                 CharacterAgent.SetDestination(MouseHit.point);
                 CharacterAnimator.CrossFadeInFixedTime("Running",0f);
                 CharacterAgent.speed = 15;
-                CharacterAgent.angularSpeed = 360;
+                CharacterAgent.angularSpeed = 3600;
                 NeedsToWalk = true;
                 CharacterAgent.velocity = CharacterAgent.velocity*10;
+               if(CurrentStamina <1)
+               {
+                   IsRunning = false;
+
+               } 
+                IsRunning = true;
             }    
         }
     }
-
+    void StaminaRecharge()
+    {
+        if (IsRunning)
+     {
+         CurrentStamina = Mathf.Clamp(CurrentStamina - (StaminaDecreasePerFrame * Time.deltaTime), 0.0f, MaxStamina);
+         StaminaRechargeDelayTimer = 0.0f;
+     }
+     else if (CurrentStamina < MaxStamina)
+     {
+         if (StaminaRechargeDelayTimer >= StaminaTimeToRegen)
+             CurrentStamina = Mathf.Clamp(CurrentStamina + (StaminaIncreasePerFrame * Time.deltaTime), 0.0f, MaxStamina);
+         else
+             StaminaRechargeDelayTimer += Time.deltaTime;
+     }
+    }
+    void OnGUI()
+    {
+        float ratio = CurrentStamina / MaxStamina;
+        float rectWidth = ratio*Screen.width /3;
+        StaminaRectangle.width =rectWidth;
+        GUI.DrawTexture(StaminaRectangle, StaminaTexture);
+    }
 
 }
--- gj-vln-2020/Assets/Scripts/MainMenuButtons.cs
+++ gj-vln-2020/Assets/Scripts/MainMenuButtons.cs
@@ -6,64 +6,64 @@ using UnityEngine.UI;
 
 public class MainMenuButtons : MonoBehaviour
 {
-    public GameObject MainButtons;
-    public GameObject OptionsMenu;
+//     public GameObject MainButtons;
+//     public GameObject OptionsMenu;
 
-    [SerializeField]
-    private Slider volumeSlider;
-    [SerializeField]
-    private Toggle fullScreenToggle;
-    [SerializeField]
-    private Sprite ResumeSprite;
-    [SerializeField]
-    private Button StartBtn;
+//     [SerializeField]
+//     private Slider volumeSlider;
+//     [SerializeField]
+//     private Toggle fullScreenToggle;
+//     [SerializeField]
+//     private Sprite ResumeSprite;
+//     [SerializeField]
+//     private Button StartBtn;
 
-    void Start()
-    {
-        if (File.Exists(Application.persistentDataPath + "/pdata.sn"))
-        {
-            StartBtn.image.sprite = ResumeSprite;
-        }
-    }
+//     void Start()
+//     {
+//         if (File.Exists(Application.persistentDataPath + "/pdata.sn"))
+//         {
+//             StartBtn.image.sprite = ResumeSprite;
+//         }
+//     }
 
-    public void StartClicked()
-    {
-        //load next scene
-    }
+//     public void StartClicked()
+//     {
+//         //load next scene
+//     }
 
-    public void OptionsClicked()
-    {
-        MainButtons.SetActive(false);
-        OptionsMenu.SetActive(true);
-        volumeSlider.value = Game.current.Volume;
-        fullScreenToggle.isOn = Game.current.FullScreen;
-    }
+//     public void OptionsClicked()
+//     {
+//         MainButtons.SetActive(false);
+//         OptionsMenu.SetActive(true);
+//         volumeSlider.value = Game.current.Volume;
+//         fullScreenToggle.isOn = Game.current.FullScreen;
+//     }
 
-    public void ExitClicked()
-    {
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#else
-        Application.Quit();
-#endif
-    }
+//     public void ExitClicked()
+//     {
+// #if UNITY_EDITOR
+//         UnityEditor.EditorApplication.isPlaying = false;
+// #else
+//         Application.Quit();
+// #endif
+//     }
 
-    public void ChangeVolume()
-    {
-        AudioListener.volume = volumeSlider.value;
-        Game.current.Volume = volumeSlider.value;
-    }
+//     public void ChangeVolume()
+//     {
+//         AudioListener.volume = volumeSlider.value;
+//         Game.current.Volume = volumeSlider.value;
+//     }
 
-    public void ToggleFullScreen()
-    {
-        Screen.SetResolution(Screen.width, Screen.height, fullScreenToggle.isOn);
-        Game.current.FullScreen = fullScreenToggle.isOn;
-    }
+//     public void ToggleFullScreen()
+//     {
+//         Screen.SetResolution(Screen.width, Screen.height, fullScreenToggle.isOn);
+//         Game.current.FullScreen = fullScreenToggle.isOn;
+//     }
 
-    public void BackClicked()
-    {
-        Game.SaveGame();
-        OptionsMenu.SetActive(false);
-        MainButtons.SetActive(true);
-    }
+//     public void BackClicked()
+//     {
+//         Game.SaveGame();
+//         OptionsMenu.SetActive(false);
+//         MainButtons.SetActive(true);
+//     }
 }
