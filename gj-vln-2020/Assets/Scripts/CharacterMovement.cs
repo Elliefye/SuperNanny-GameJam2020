e52a47b4b482e07df62e43f41ce1a5f8098b8bb9
EnZeeHI <34064073+EnZeeHI@users.noreply.github.com>
2020-02-01 05:21:37 +0200

    Buggy running

--- gj-vln-2020/Assets/Scripts/CharacterMovement.cs
+++ gj-vln-2020/Assets/Scripts/CharacterMovement.cs
@@ -7,6 +7,10 @@ public class CharacterMovement : MonoBehaviour
 {
 
     public LayerMask whatCanBeClickedOn;
+    public int MaxStamina;
+    public int CurrentStamina;
+    public int StaminaRechargeDelay;
+    public int StaminaRechargeRate;
     private NavMeshAgent CharacterAgent;
 
     bool NeedsToWalk;
@@ -17,6 +21,7 @@ public class CharacterMovement : MonoBehaviour
     {
         CharacterAgent = GetComponent<NavMeshAgent>();
         CharacterAnimator = GetComponent<Animator>();
+        CurrentStamina = MaxStamina;
         //NeedsToWalk = false;
     }
 
@@ -24,6 +29,7 @@ public class CharacterMovement : MonoBehaviour
     void Update()
     {   // casting ray once from the mouse position
         Ray MouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+     
         
         //checking for collisions
         if (Input.GetKeyDown(KeyCode.Mouse0))
@@ -35,8 +41,13 @@ public class CharacterMovement : MonoBehaviour
             {  
                 CharacterAgent.SetDestination(MouseHit.point);
                 CharacterAnimator.CrossFadeInFixedTime("Walking",0f);
+                CharacterAgent.speed = 10;
                 NeedsToWalk = true;
             }    
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            Running(MouseRay);
         }
         // checking if the player reached the destination and switching to idle animation
         if (!CharacterAgent.pathPending)
@@ -52,4 +63,22 @@ public class CharacterMovement : MonoBehaviour
             }
         } 
     }
+
+    void Running(Ray ray)
+    {    RaycastHit MouseHit;
+        if (CurrentStamina > 1)
+        {
+            if (Physics.Raycast(ray, out MouseHit, 100, whatCanBeClickedOn))
+            {  
+                CharacterAgent.SetDestination(MouseHit.point);
+                CharacterAnimator.CrossFadeInFixedTime("Running",0f);
+                CharacterAgent.speed = 15;
+                CharacterAgent.angularSpeed = 360;
+                NeedsToWalk = true;
+                CharacterAgent.velocity = CharacterAgent.velocity*10;
+            }    
+        }
+    }
+
+
 }
